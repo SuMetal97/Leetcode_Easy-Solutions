f0568d482b0fb0d75259a9233ef9e16c8b7eb609
Dakota Wrigley <43604399+SuMetal97@users.noreply.github.com>
2022-09-10 17:20:02 -0400

    Create leetcode_70.c

---
+++ leetcode_70.c
@@ -0,0 +1,14 @@
+int climbStairs(int n){
+    int *ans; 
+    ans= malloc(50*sizeof(int *));
+    ans[0] = 0;
+    ans[1] = 1;
+    ans[2] = 2;
+    
+    for( int  i = 3; i <= n; i++){
+        ans[i] = ans[i-1] + ans[i-2];
+    }
+    int answer = ans[n];
+    free(ans);
+    return answer;
+}
